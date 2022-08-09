b5a4107a920641ba1af51ba46021cd14766918c2
bjhardcastle <ben.hardcastle@alleninstitute.org>
2022-08-08 17:55:05 -0700

    Update bat file for launching

--- clear_dirs.bat
+++ clear_dirs.bat
@@ -1,6 +1,10 @@
+@REM if necessary, clone repo with:
+@REM git clone https://github.com/AllenInstitute/np_data_validation.git
 git checkout main
 git pull origin main
 
+@REM if environment doesn't exist, create it with:
+@REM conda create --file environment.yml
 CALL conda activate dv
 
 python data_validation.py
