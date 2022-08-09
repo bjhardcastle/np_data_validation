fe50008e9863108fc6d30effa1429f103cce8fda
svc_neuropix <svc_neuropix@alleninstitute.org>
2022-08-08 17:54:02 -0700

    Update env and bat files

--- clear_dirs.bat
+++ clear_dirs.bat
@@ -1,6 +1,11 @@
+ECHO off
+title Checking for valid backups and clearing local directories
+
 git checkout main
 git pull origin main
 
+CALL conda env create --file environment.yml
 CALL conda activate dv
 
-python data_validation.py
+CALL python data_validation.py
+cmd \k
