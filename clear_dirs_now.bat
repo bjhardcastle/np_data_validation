a711921adb88c1f00b10281fd28df69ac8eb43a4
bjhardcastle <ben.hardcastle@alleninstitute.org>
2022-08-10 23:35:55 -0700

    Update batch file after testing

--- clear_dirs_now.bat
+++ clear_dirs_now.bat
@@ -1,7 +1,7 @@
 @REM if necessary, clone repo with:
 @REM git clone https://github.com/AllenInstitute/np_data_validation.git
 git checkout main
-git pull origin main
+git pull 
 
 @REM if environment doesn't exist, create it with:
 @REM conda create --file environment.yml
